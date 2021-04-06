2db542c02720e4d3634b8ab92b2dc2968fe0c300
xuming624 <xuming624@qq.com>
2021-04-06 21:24:01 +0800

    update readme.

--- examples/userdict.py
+++ examples/userdict.py
@@ -16,6 +16,7 @@ print(d.user_sentiment_dict)
 a_sentence = ['剁椒鸡蛋好难吃。绝对没人受得了',
               '土豆丝很好吃', '土豆丝很难吃',
               '这笔钱是个天文数字',
+              '啥也不是',
               '我一会儿出去玩了，你吃啥？给你带,然而你不知道']
 for i in a_sentence:
     r = d.classify(i)
