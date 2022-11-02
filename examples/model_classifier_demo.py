3d8e89f00e73b90f442a5f2f17a5c1f85fd72419
shibing624 <shibing624@126.com>
2022-11-02 18:08:42 +0800

    update case.

--- examples/model_classifier_demo.py
+++ examples/model_classifier_demo.py
@@ -7,6 +7,8 @@ from pysenti import ModelClassifier
 
 texts = ["苹果是一家伟大的公司",
          "土豆丝很好吃",
+         "垃圾，在酒店中应该是很差的！",
+         "我们刚走过一个烧烤店",
          "土豆丝很难吃"]
 
 if __name__ == '__main__':
