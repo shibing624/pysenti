31e5e229600e52409c9b10b680754ff854f176ba
shibing624 <shibing624@126.com>
2023-01-09 14:59:02 +0800

    update rule sentiment demo.

--- examples/rule_classifier_demo.py
+++ examples/rule_classifier_demo.py
@@ -6,9 +6,12 @@
 import pysenti
 
 if __name__ == '__main__':
-    texts = ["苹果是一家伟大的公司",
-             "土豆丝很好吃",
-             "土豆丝很难吃"]
+    texts = [
+        "这个电影真心很难看，大多数人都不喜欢，被骂很久，早就习惯了。",
+        "苹果是一家伟大的公司",
+        "土豆丝很好吃",
+        "土豆丝很难吃"
+    ]
     for i in texts:
         r = pysenti.classify(i)
         print(i, r['score'])
